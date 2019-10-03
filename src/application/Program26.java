69367aab024f8a756fac2baacc6cc3e4e922fc08
Antônio B. M. Filho <benonimonteirog@gmail.com>
2019-10-03 08:17:50 -0300

    Exeções

---
+++ src/application/Program26.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program26 {
+	
+	public static void main(String[] args) {
+	
+		method1();
+		System.out.println("end of program!");
+	}
+	
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");		
+		method2();		
+		System.out.println("***METHOD1 END***");
+	}
+	
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			
+			System.out.println(vect[position]);			
+		} 
+		catch (InputMismatchException e) {
+			System.out.println("input error");
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("invalid position");
+			//e.printStackTrace();
+			//sc.next();
+		}	
+		
+		sc.close();
+		System.out.println("***METHOD2 END***");
+	}
+
+}
